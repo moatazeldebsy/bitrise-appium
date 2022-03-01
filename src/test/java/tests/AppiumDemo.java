bd612047d3b94671a647c8bceebdf0f9fea5ea5c
moatazeldebsy <moataznabil2019@gmail.com>
2022-03-01 18:36:54 +0100

    changes for the demo

--- src/test/java/tests/AppiumDemo.java
+++ src/test/java/tests/AppiumDemo.java
@@ -2,6 +2,8 @@ package tests;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -18,23 +20,26 @@ public class AppiumDemo {
     public void Android_setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("automationName", "espresso");
-        capabilities.setCapability("platformVersion", "10.0");
+        capabilities.setCapability("platformVersion", "11.0");
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("showGradleLog",true);
         capabilities.setCapability("app",
-                System.getProperty("user.dir") + "/apps/app-debug.apk");
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
+                System.getProperty("user.dir") + "/apps/Calculator.apk");
+        driver = new AndroidDriver(new URL("http://localhost:4724/wd/hub"), capabilities);
     }
 
     @Test
-    public void calcDemo(){
-        driver.findElementById("buttonOne").click();
-        driver.findElementById("buttonAdd").click();
-        driver.findElementById("buttonTwo").click();
-        driver.findElementById("buttonEqual").click();
-        Assert.assertEquals(driver.findElementById("editText").getText(),"3");
+    public void calcDemo() throws InterruptedException {
+        WebElement number1 = driver.findElementById("digit_1");
+        WebElement plus =  driver.findElementById("op_add");
+        WebElement number2 = driver.findElementById("digit_2");
+        WebElement equal =  driver.findElementById("eq");
 
+        number1.click();
+        plus.click();
+        number2.click();
+        equal.click();
     }
 
 
